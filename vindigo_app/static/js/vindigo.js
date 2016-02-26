03ffbeb6ad1a38b99d52572c3d98e2757017ed2b
taapesh <starmansix@gmail.com>
2016-02-26 00:39:18 -0600

    Pulling device_id correctly now

--- vindigo_app/static/js/vindigo.js
+++ vindigo_app/static/js/vindigo.js
@@ -58,48 +58,18 @@ $(function() {
     tripStats.hide();
 
     $.ajax({
-        // The 'type' property sets the HTTP method.
-        // A value of 'PUT' or 'DELETE' will trigger a preflight request.
         type: 'GET',
-        dataType: 'jsonp',
-
-        // The URL to make the request to.
         url: getDevicesBase,
 
-        // The 'contentType' property sets the 'Content-Type' header.
-        // The JQuery default for this property is
-        // 'application/x-www-form-urlencoded; charset=UTF-8', which does not trigger
-        // a preflight. If you set this value to anything other than
-        // application/x-www-form-urlencoded, multipart/form-data, or text/plain,
-        // you will trigger a preflight request.
-        // contentType: 'text/plain',
-
-        xhrFields: {
-            // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
-            // This can be used to set the 'withCredentials' property.
-            // Set the value to 'true' if you'd like to pass cookies to the server.
-            // If this is enabled, your server must respond with the header
-            // 'Access-Control-Allow-Credentials: true'.
-            withCredentials: false
-        },
-
-        headers: {
-            // Set any custom headers here.
-            // If you set any non-simple headers, your server must include these
-            // headers in the 'Access-Control-Allow-Headers' response header.
-            'Access-Control-Allow-Origin': '*'
-        },
-
-        success: function() {
-            // Here's where you handle a successful response.
-            console.log('success');
+        success: function(data) {
+            var listItems = '';
+            for (var i = 0; i < data.length; i++) {
+                listItems += "<option value='" + i + "'>" + data[i].device_id + "</option>";
+            }
+            deviceSelect.append(listItems);
         },
 
         error: function() {
-            // Here's where you handle an error response.
-            // Note that if the error was due to a CORS issue,
-            // this function will still fire, but there won't be any additional
-            // information about the error.
             console.log('error');
         }
     });
